6793d1443b2eb0db19c610fd5404a2ede7647bd0
svr <svr@protonmail.com>
2021-12-17 09:32:42 +0300

    fragment working stub

---
+++ app/src/main/java/com/geekbrains/potd/view/picture/POTDFragment.kt
@@ -0,0 +1,27 @@
+package com.geekbrains.potd.view.picture
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.geekbrains.potd.databinding.FragmentMainBinding
+
+class POTDFragment : Fragment() {
+    private var _binding: FragmentMainBinding? = null
+    val binding: FragmentMainBinding get() = _binding!!
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentMainBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}

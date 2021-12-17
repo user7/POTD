5624bc418dd6ca8d43ff073ff29676be96f2a4e3
svr <svr@protonmail.com>
2021-12-17 09:20:44 +0300

    added strings, colors, drawables, fragment layout, simplified main layout

--- app/src/main/java/com/geekbrains/potd/view/MainActivity.kt
+++ app/src/main/java/com/geekbrains/potd/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.geekbrains.potd
+package com.geekbrains.potd.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.geekbrains.potd.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {

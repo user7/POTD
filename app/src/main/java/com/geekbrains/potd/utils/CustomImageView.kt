1027745f16f46ac49b08393e30dc6f892243f242
svr <svr@protonmail.com>
2021-12-17 08:51:05 +0300

    overriding onMeasure to make view square

--- app/src/main/java/com/geekbrains/potd/utils/CustomImageView.kt
+++ app/src/main/java/com/geekbrains/potd/utils/CustomImageView.kt
@@ -3,7 +3,13 @@ package com.geekbrains.potd.utils
 import android.content.Context
 import android.util.AttributeSet
 import androidx.appcompat.widget.AppCompatImageView
+import java.lang.Integer.min
 
 class CustomImageView @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, style: Int = 0):
     AppCompatImageView(context, attrs, style)
-{}
+{
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        val m = min(widthMeasureSpec, heightMeasureSpec)
+        super.onMeasure(m, m)
+    }
+}

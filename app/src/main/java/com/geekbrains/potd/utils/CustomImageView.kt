cd782f978800e72dd16cd6ff24c47c19400e55cb
svr <svr@protonmail.com>
2021-12-17 08:46:55 +0300

    added CutomImageView stub

---
+++ app/src/main/java/com/geekbrains/potd/utils/CustomImageView.kt
@@ -0,0 +1,9 @@
+package com.geekbrains.potd.utils
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatImageView
+
+class CustomImageView constructor(context: Context, attrs: AttributeSet? = null, style: Int = 0):
+    AppCompatImageView(context, attrs, style)
+{}

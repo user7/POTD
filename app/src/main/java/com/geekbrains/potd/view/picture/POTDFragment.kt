ce049e989bb230c14f300c8deed5c2f288cd0dc7
svr <svr@protonmail.com>
2021-12-22 17:49:13 +0300

    1.misc: description in bottom sheet layout

--- app/src/main/java/com/geekbrains/potd/view/picture/POTDFragment.kt
+++ app/src/main/java/com/geekbrains/potd/view/picture/POTDFragment.kt
@@ -5,9 +5,7 @@ import android.net.Uri
 import android.os.Bundle
 import android.view.*
 import android.widget.Toast
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import coil.load
 import com.geekbrains.potd.R
@@ -15,7 +13,7 @@ import com.geekbrains.potd.databinding.FragmentMainBinding
 import com.geekbrains.potd.view.MainActivity
 import com.geekbrains.potd.viewmodel.POTDState
 import com.geekbrains.potd.viewmodel.POTDViewModel
-import com.google.android.material.bottomappbar.BottomAppBar
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 
 class POTDFragment : Fragment() {
     private var _binding: FragmentMainBinding? = null
@@ -55,6 +53,8 @@ class POTDFragment : Fragment() {
                 val url = state.potdResponse.url
                 binding.imageView.load(url) {}
                 binding.imageDescription.setText(state.potdResponse.title)
+                binding.includeBottomSheet.bottomSheetDescriptionHeader.text =
+                    state.potdResponse.explanation
             }
         }
     }
@@ -94,28 +94,20 @@ class POTDFragment : Fragment() {
         return true
     }
 
-    private var isMainScreen = true
+    private var isCollapsed = true
     private fun setBottomAppBar() {
         val context = activity as MainActivity
         context.setSupportActionBar(binding.bottomAppBar)
         setHasOptionsMenu(true)
 
         binding.fab.setOnClickListener {
-            if (isMainScreen) {
-                binding.bottomAppBar.navigationIcon = null
-                binding.bottomAppBar.fabAlignmentMode = BottomAppBar.FAB_ALIGNMENT_MODE_END
-                binding.fab.setImageDrawable(ContextCompat.getDrawable(context,
-                    R.drawable.ic_back_fab))
-                binding.bottomAppBar.replaceMenu(R.menu.menu_bottom_bar_other_screen)
+            val behavior = BottomSheetBehavior.from(binding.includeBottomSheet.bottomSheetContainer)
+            if (isCollapsed) {
+                behavior.state = BottomSheetBehavior.STATE_EXPANDED
             } else {
-                binding.bottomAppBar.navigationIcon = ContextCompat.getDrawable(context,
-                    R.drawable.ic_hamburger_menu_bottom_bar)
-                binding.bottomAppBar.fabAlignmentMode = BottomAppBar.FAB_ALIGNMENT_MODE_CENTER
-                binding.fab.setImageDrawable(ContextCompat.getDrawable(context,
-                    R.drawable.ic_plus_fab))
-                binding.bottomAppBar.replaceMenu(R.menu.menu_bottom_bar)
+                behavior.state = BottomSheetBehavior.STATE_COLLAPSED
             }
-            isMainScreen = !isMainScreen
+            isCollapsed = !isCollapsed
         }
     }
 }

fbd96435b25dc8ac2d62dffae26c730d9abc7959
svr <svr@protonmail.com>
2022-01-26 18:20:30 +0300

    Тесты data class + Gson

---
+++ app/src/main/java/com/geekbrains/potd/experiments/DataClassB.kt
@@ -0,0 +1,3 @@
+package com.geekbrains.potd.experiments
+
+data class DataClassB(val x: Int, val dataA: DataClassA?) : DataClassBase()
---
+++ app/src/main/java/com/geekbrains/potd/experiments/DataClassBase.kt
@@ -0,0 +1,5 @@
+package com.geekbrains.potd.experiments
+
+open class DataClassBase : Cloneable {
+    public override fun clone(): Any = super.clone()
+}

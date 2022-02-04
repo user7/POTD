30cae7617f740355360b23cdc083657fd86fc92a
svr <svr@protonmail.com>
2022-02-04 15:14:15 +0300

    Анимация в SplashActivity

--- app/src/main/java/com/geekbrains/potd/SplashActivity.kt
+++ app/src/main/java/com/geekbrains/potd/SplashActivity.kt
@@ -4,13 +4,23 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
+import com.geekbrains.potd.databinding.ActivityMainBinding
+import com.geekbrains.potd.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
     private val handler: Handler by lazy { Handler(mainLooper) }
+    private lateinit var binding: ActivitySplashBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.star1.animate().rotationBy(100f).duration = 2100
+        binding.star2.animate().rotationBy(-100f).duration = 2100
+        binding.star3.animate().rotationBy(100f).duration = 2100
+
         handler.postDelayed(
             {
                 startActivity(Intent(this@SplashActivity, MainActivity::class.java))
